97c715688b619bded800c70242cdaf8a125d3cd3
yjl <990735679@qq.com>
2017-02-19 23:27:24 +0800

    service function login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -21,8 +21,8 @@ export class LoginComponent implements  OnInit {
   public doLogin():void{
     this.loginService.login(this.user);
   }
-  public validUserName(form,userName){
-    console.log(form,userName);
-  }
+  // public validUserName(form,userName){
+  //   console.log(form,userName);
+  // }
 
 }
--- src/app/user/login/login.service.ts
+++ src/app/user/login/login.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Http,Response} from "@angular/http";
 
 @Injectable()
 export  class LoginService{
+  public loginUrl = 'mock-data/user-login-mock.json';
+  constructor(public http:Http){
+
+  }
   login(user){
     console.log(user);
+   return  this.http.post(this.loginUrl,user).map((response:Response)=>{
+      let user = response.json();
+      console.log('user data',user);
+      if (user && user.token){
+
+      }
+      return response;
+   }) .subscribe(data=>console.log('login success',data),error=>console.log(error))
   }
 }
